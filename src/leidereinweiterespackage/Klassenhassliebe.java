afb6b66759b3aba2f630fe7b1b828f1e15707a16
Bochejny Nicolas <1810653904@stud.fh-kufstein.ac.at>
2018-11-08 14:13:52 +0100

    Jetzt sogar mit Altersfreigabe

---
+++ src/leidereinweiterespackage/Klassenhassliebe.java
@@ -0,0 +1,9 @@
+package leidereinweiterespackage;
+
+public class Klassenhassliebe {
+    public static void main (String [] args){
+        String name = ("Nicolas Bochejny");
+        int alter = (20);
+        System.out.println(name + " " + alter);
+    }
+}
