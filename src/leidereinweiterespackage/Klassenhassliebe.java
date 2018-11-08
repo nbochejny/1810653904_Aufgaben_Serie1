2fac5e184018e365bfd4a81f9441686206626c38
Bochejny Nicolas <1810653904@stud.fh-kufstein.ac.at>
2018-11-08 14:42:38 +0100

    Ausfüllen und so

--- src/leidereinweiterespackage/Klassenhassliebe.java
+++ src/leidereinweiterespackage/Klassenhassliebe.java
@@ -1,10 +1,11 @@
 package leidereinweiterespackage;
 
 public class Klassenhassliebe {
-    public static void main (String [] args){
+    public static void main(String[] args) {
         String name = ("Nicolas Bochejny");
         int alter = (20);
         final String FH_KUFSTEIN_TIROL = ("Fachhochschule Kufstein Tirol"); //Gekürzt auf fhKufsteinTirol & in Capslock da es final ist
-        System.out.println(name + " " + alter + " " + FH_KUFSTEIN_TIROL);
+        final String Ort = ("Rosenheim");
+        System.out.println(name + " " + alter + " " + Ort + " " + FH_KUFSTEIN_TIROL);
     }
 }
