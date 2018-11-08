766a477985b4edbf841820ab73cd79dafbaaab77
Bochejny Nicolas <1810653904@stud.fh-kufstein.ac.at>
2018-11-08 14:04:00 +0100

    Kommentare hinzugefügt

--- src/ichbineinpackage/Klassenfreund.java
+++ src/ichbineinpackage/Klassenfreund.java
@@ -1,7 +1,7 @@
-package ichbineinpackage;
+package ichbineinpackage; //Zweites Package wie in serlo
 
-public class Klassenfreund {
-    public static void main(String [] args){
-        System.out.println("FH Kufstein Tirol");
+public class Klassenfreund { //wie in Übung1
+    public static void main(String [] args){ //wie in Übung1
+        System.out.println("FH Kufstein Tirol"); //wie in Übung1
     }
 }
--- src/serlo/Übung1.java
+++ src/serlo/Übung1.java
@@ -1,10 +1,10 @@
-package serlo;
+package serlo; //Erstes Package in welcher die Klasse ist
 
-public class Übung1{
+public class Übung1{ //Klasse in welcher eine main Methode ist
 
-    public static void main(String[] args) {
-        String vorname = ("Nicolas Bochejny");
+    public static void main(String[] args) { //Die main Methode wodurch das Java weiß wo das Programm anfängt(main ist der Name, in der geschweiften Klammer ist die Methode definiert)
+        String vorname = ("Nicolas Bochejny"); //String um einen Wert anzugeben (bsp. Buchstaben und Zeichen), vorname als Objekt mit dem Wert "Nicolas Bochejny" definiert
 
-     System.out.println("Hello World " + vorname);
+     System.out.println("Hello World " + vorname); //system... zum ausgeben der in Klammern stehenden Werte
     }
 }
